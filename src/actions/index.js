d515b5c1048d98b74d05916fce59b825885b43aa
glen <jinx3y@gmail.com>
2018-03-18 17:50:53 +0100

    added reference to keys/api_keys.js file "apiKeys.OpenWeatherApiKey"

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import apiKeys from '../keys/api_keys';
 
-const OPEN_WEATHER_API_KEY = '5fa998c8439776c11ccff3aa4e462972';
+const OPEN_WEATHER_API_KEY = apiKeys.OpenWeatherApiKey ;
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${OPEN_WEATHER_API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
